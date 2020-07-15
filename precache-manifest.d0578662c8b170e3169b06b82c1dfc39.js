ba75b51de16d9a1f29b1205110d70c263f31498d
hobbyluv <princewillchime43@gmail.com>
2020-07-15 22:28:04 +0100

    Updates

---
+++ precache-manifest.d0578662c8b170e3169b06b82c1dfc39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a733255f899a389c4bd5b23d3b3da7f",
+    "url": "/food-recipe-application/index.html"
+  },
+  {
+    "revision": "d2fb36d3297c9e94a43e",
+    "url": "/food-recipe-application/static/css/main.0b3f7b67.chunk.css"
+  },
+  {
+    "revision": "930aa455bfa7c68b2ba9",
+    "url": "/food-recipe-application/static/js/2.68483a14.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/food-recipe-application/static/js/2.68483a14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2fb36d3297c9e94a43e",
+    "url": "/food-recipe-application/static/js/main.dbac5668.chunk.js"
+  },
+  {
+    "revision": "13e891c3ac21ca59eff4",
+    "url": "/food-recipe-application/static/js/runtime-main.14574123.js"
+  }
+]);
