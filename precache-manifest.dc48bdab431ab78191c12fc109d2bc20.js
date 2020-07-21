3aecc901c3ceac5bdc2d7fb9c465a55182dc6259
hobbyluv <princewillchime43@gmail.com>
2020-07-21 13:20:01 +0100

    Updates

--- precache-manifest.dc48bdab431ab78191c12fc109d2bc20.js
+++ precache-manifest.dc48bdab431ab78191c12fc109d2bc20.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8120ddfec0dcb58a6848c95709b69677",
+    "revision": "d98f64875ba1a332c06ad45e9ac8539c",
     "url": "/food-recipe-application/index.html"
   },
   {
-    "revision": "4b6a1773035410a6987e",
-    "url": "/food-recipe-application/static/css/main.0b3f7b67.chunk.css"
+    "revision": "09922d1cbd073cfa85b4",
+    "url": "/food-recipe-application/static/css/main.491c0cc8.chunk.css"
   },
   {
     "revision": "930aa455bfa7c68b2ba9",
@@ -16,11 +16,15 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/food-recipe-application/static/js/2.68483a14.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4b6a1773035410a6987e",
+    "revision": "09922d1cbd073cfa85b4",
     "url": "/food-recipe-application/static/js/main.022a834d.chunk.js"
   },
   {
     "revision": "13e891c3ac21ca59eff4",
     "url": "/food-recipe-application/static/js/runtime-main.14574123.js"
+  },
+  {
+    "revision": "459ffe82cef861afdc52b3031b179bf0",
+    "url": "/food-recipe-application/static/media/recipeima.459ffe82.jpeg"
   }
 ]);
